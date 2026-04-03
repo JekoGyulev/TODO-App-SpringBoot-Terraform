82fdb4b5194dfb4bee9340ddbc45b16cddc142c7
JekoGyulev <165460153+JekoGyulev@users.noreply.github.com>
2026-04-03 11:29:48 +0300

    Initial repo commit

---
+++ src/test/java/com/example/todoappterraform/TodoAppTerraformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoappterraform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppTerraformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
